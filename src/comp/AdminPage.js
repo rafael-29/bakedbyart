6a061809a0f78ab31b03baabc8388bcb52ed27bb
rafuu <rafaelsobook@gmail.com>
2021-02-04 23:54:10 +0400

    updating admin page

--- src/comp/AdminPage.js
+++ src/comp/AdminPage.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const recent = 'recent'
 const finish = 'finish'
@@ -7,10 +7,10 @@ const finish = 'finish'
 const AdminPage = () => {
 
 const [page, setPage] = useState(recent)
+const [allOrders, setAllOrders] = useState()
+const [finishOrder, setAllFinishOrder] = useState()
 
-//const passw = props.theprops.match.params.pass
-const passw = localStorage.getItem('adminauth')
-if(!passw) return window.location.replace('/signin')
+
 
 const signOut = () => {
 localStorage.clear()
@@ -18,10 +18,111 @@ window.location.replace('/signin')
 }
 
 
+
+
+const retrieveOrders = () => {
+    axios.get('https://bakedbyartapi.herokuapp.com/orders')
+    .then( rslts => {
+    const data = rslts.data;
+    setAllOrders(data)
+    })
+    .catch(err => console.log(err))
+}
+
+const retrieveFinishOrder = () => {
+    axios.get('https://bakedbyartapi.herokuapp.com/finish')
+    .then( rslts => {
+    const data = rslts.data;
+    setAllFinishOrder(data)
+    })
+    .catch(err => console.log(err))
+}
+
+const deleteOrder = (del) => {
+axios.delete(`https://bakedbyartapi.herokuapp.com/orders/${del._id}`)
+.then( () => retrieveOrders())
+.catch(err => console.log(err))
+
+}
+
+// SAVE TO FINISHED ORDER
+const saveToFinish = (ord) => {
+axios.post('https://bakedbyartapi.herokuapp.com/finish/add', ord)
+.then( () => {
+retrieveFinishOrder()
+})
+.catch(err => console.log(err))
+}
+
+const renderAllOrders = () => {
+return allOrders.map(order => (
+<div className="order-bx" key={order.orderno}>
+    <div className="order">{order.orderno}</div>
+    <div className="order">{new Date(order.orderdate).toLocaleDateString('en-US',{
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+    })}</div>
+    <div className="order">{new Date(order.useraddress.date).toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+    }) }</div>
+    <div className="order">{order.total.totalwship}</div>
+    <div className="order order-btnz">
+        <button onClick={() => saveToFinish(order)} className="ol-btn ol-btn-check"><i className="far fa-check-circle"></i></button>
+        <button onClick={() => deleteOrder(order)} className="ol-btn"><i className="far fa-times-circle"></i></button> 
+    </div>
+</div>
+))
+}
+
+const deleteFinishOrder = (del) => {
+axios.delete(`https://bakedbyartapi.herokuapp.com/finish/${del.orderno}`)
+.then( () => retrieveFinishOrder())
+.catch(err => console.log(err))
+}
+
+const renderFinishOrders = () => {
+    return finishOrder.map(order => (
+    <div className="order-bx" key={order.orderno}>
+        <div className="order">{order.orderno}</div>
+        <div className="order">{new Date(order.orderdate).toLocaleDateString('en-US',{
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+        })}</div>
+        <div className="order">{new Date(order.useraddress.date).toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+        }) }</div>
+        <div className="order">{order.total.totalwship}</div>
+        <div className="order order-btnz">
+            <button onClick={() => deleteFinishOrder(order)} className="ol-btn"><i className="far fa-times-circle"></i></button> 
+        </div>
+    </div>
+    ))
+}
+
+
 const renderRecent = () => (
 <div className="recent-page">
     <div className="dashfield-ttlebx">
         <h2 className="dash-field-ttle">Recent Orders</h2>
+    </div>
+    <div className="orderlist-container">
+        <div className="orderlist-headerz">
+            <div className="ol-header">order no.</div>
+            <div className="ol-header">order date</div>
+            <div className="ol-header">delivery date</div>
+            <div className="ol-header">total</div>
+            <div className="ol-header">actions</div>
+        </div>
+        <div className="o-container">
+        {allOrders === undefined || !allOrders.length ? <>No Orders as of yet</> : renderAllOrders()}
+        </div>
+        
     </div>
 </div>
 )
@@ -31,10 +132,29 @@ const renderFinish = () => (
     <div className="dashfield-ttlebx">
         <h2 className="dash-field-ttle">Delivered Orders</h2>
     </div>
+    <div className="orderlist-container">
+        <div className="orderlist-headerz">
+            <div className="ol-header">order no.</div>
+            <div className="ol-header">order date</div>
+            <div className="ol-header">delivery date</div>
+            <div className="ol-header">total</div>
+            <div className="ol-header">actions</div>
+        </div>
+        <div className="o-container">
+        {finishOrder === undefined || !finishOrder.length ? <>NO DELIVERED ORDERS</> : renderFinishOrders()}
+        </div>
+        
+    </div>
 </div>
 )
 
+useEffect(() => {
+retrieveOrders()
+retrieveFinishOrder()
+}, [])
 
+const passw = localStorage.getItem('adminauth')
+if(!passw) return window.location.replace('/signin')
 /////// START OF JSX
 const renderAdminPage = () =>  (
     <div className="admin-page">
@@ -74,7 +194,7 @@ const renderAdminPage = () =>  (
         </div>
     
         <div className="dash-field">
-            <button onClick={signOut} className="signout-btn">Sign Out</button>
+        <button onClick={signOut} className="signout-btn">Sign Out</button>
         {page === recent ? renderRecent() : renderFinish()}
 
         </div>
@@ -82,9 +202,6 @@ const renderAdminPage = () =>  (
     </div>
 )
 
-const returnHome = () => {
-window.location.replace('/signin')
-}
 return(
 <>
 {renderAdminPage()}
